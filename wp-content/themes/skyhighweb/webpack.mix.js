3074f5af89f6244e3bd7836f5a16bc190b5f1005
Ilia Griniuk <44933254+sky0812@users.noreply.github.com>
2024-10-09 23:30:41 +0200

    initial commit
    Signed-off-by: ilia <iliagriniuk@gmail.com>

---
+++ wp-content/themes/skyhighweb/webpack.mix.js
@@ -0,0 +1,46 @@
+let mix = require('laravel-mix');
+let path = require('path');
+
+mix.setResourceRoot('../');
+mix.setPublicPath(path.resolve('./'));
+
+mix.webpackConfig({
+  watchOptions: {
+    ignored: [
+      path.posix.resolve(__dirname, './node_modules'),
+      path.posix.resolve(__dirname, './css'),
+      path.posix.resolve(__dirname, './js')
+    ]
+  }
+});
+
+mix.js('resources/js/app.js', 'js');
+
+mix.postCss("resources/css/app.css", "css");
+
+mix.postCss("resources/css/editor-style.css", "css");
+
+mix.browserSync({
+  proxy: 'http://wpbaseline.localhost/',
+  host: 'wpbaseline.localhost',
+  open: 'external',
+  notify: false,
+  injectChanges: true,
+  files: [
+    {
+      match: [
+        "**/*.php",
+        "**/resources/**",
+      ],
+      fn: function (event, file) {
+        this.reload()
+      }
+    }
+  ]
+});
+
+if (mix.inProduction()) {
+  mix.version();
+} else {
+  mix.options({manifest: false});
+}
